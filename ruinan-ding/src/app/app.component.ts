d06a59f9a2be7c1180445198c8c490cd0d5030be
Ruinan Ding <ding.r866@gmail.com>
2026-03-31 03:47:25 -0400

    feature/dev18 edited pages

--- ruinan-ding/src/app/app.component.ts
+++ ruinan-ding/src/app/app.component.ts
@@ -3,8 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   imports: [],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
   title = 'ruinan-ding';
