3438ba4adec0c136f3572e219c0ea714b4a2b24a
Ruinan Ding <ding.r866@gmail.com>
2026-03-31 04:23:23 -0400

    add file

---
+++ ruinan-ding/src/types.d.ts
@@ -0,0 +1,11 @@
+// Ambient declarations for Angular modules when node_modules is not available
+declare module '@angular/core';
+declare module '@angular/common';
+declare module '@angular/compiler';
+declare module '@angular/forms';
+declare module '@angular/platform-browser';
+declare module '@angular/platform-browser-dynamic';
+declare module '@angular/router';
+declare module 'rxjs';
+declare module 'zone.js';
+declare module 'tslib';
